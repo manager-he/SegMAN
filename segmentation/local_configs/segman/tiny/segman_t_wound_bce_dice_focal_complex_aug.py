d80ce473cf1ba43109f0093d829493f6b5f158fa
autodl26 <13907094415@163.com>
2026-04-26 20:42:34 +0800

    配置纠正

--- segmentation/local_configs/segman/tiny/segman_t_wound_bce_dice_focal_complex_aug.py
+++ segmentation/local_configs/segman/tiny/segman_t_wound_bce_dice_focal_complex_aug.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../../_base_/models/segman.py',
-    '../../_base_/fuseg_wound_data_aug.py',
+    '../../_base_/datasets/fuseg_wound_data_aug.py',
     '../../_base_/default_runtime.py',
     '../../_base_/schedules/schedule_160k_adamw.py'
 ]
